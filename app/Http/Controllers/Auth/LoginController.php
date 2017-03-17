93125573e00c1040a6fce9492c29303afa68295f
v.vitovtsev <vlad.vitovtsev@mail.ru>
2017-03-17 21:00:09 +0700

    Create start page cabinet

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/Cabinet';
 
     /**
      * Create a new controller instance.
---
+++ app/Http/Controllers/CabinetController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CabinetController extends Controller
+{
+
+    /** 
+     * CabinetController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /** Main page cabinet
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('cabinet.index');
+    }
+}
